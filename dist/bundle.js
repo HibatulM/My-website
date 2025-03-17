22c4fc5f5d5200761baf4aebaebc8be2f60731f6
HibatulM <hmuqe2@uic.edu>
2025-03-17 17:06:39 -0500

    Updating firebase files

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("\r\n// For Firebase JS SDK v7.20.0 and later, measurementId is optional\r\nconst firebaseConfig = {\r\n    apiKey: \"AIzaSyC0UmuYEWiCNIwtbQTEw8Xgg1bh7nWICu0\",\r\n    authDomain: \"mywebsite-31ced.firebaseapp.com\",\r\n    projectId: \"mywebsite-31ced\",\r\n    storageBucket: \"mywebsite-31ced.firebasestorage.app\",\r\n    messagingSenderId: \"506767137938\",\r\n    appId: \"1:506767137938:web:cce0325932049810b9da4b\",\r\n    measurementId: \"G-4450RL85JZ\"\r\n  };\n\n//# sourceURL=webpack://my-website/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC0UmuYEWiCNIwtbQTEw8Xgg1bh7nWICu0",
+    authDomain: "mywebsite-31ced.firebaseapp.com",
+    projectId: "mywebsite-31ced",
+    storageBucket: "mywebsite-31ced.firebasestorage.app",
+    messagingSenderId: "506767137938",
+    appId: "1:506767137938:web:cce0325932049810b9da4b",
+    measurementId: "G-4450RL85JZ"
+  };
